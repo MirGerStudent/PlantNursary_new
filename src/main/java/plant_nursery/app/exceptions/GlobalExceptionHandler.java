c109ddb9dc6ba1f21fdecb6c102bf36e6b36542a
MirGerStudent <mirger140503@gmail.com>
2025-02-20 11:59:38 +0600

    [Change] move file

---
+++ src/main/java/plant_nursery/app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package plant_nursery.app.exceptions;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<?> handleElementNotFoundException(ElementNotFoundException ex, WebRequest request) {
+        Map<String, String> body = new HashMap<>();
+        body.put("timestamp", new Date().toString());
+        body.put("message", ex.getReason());
+        body.put("status", String.valueOf(((Integer) HttpStatus.NOT_FOUND.value())));
+        body.put("path", request.getDescription(false));
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
