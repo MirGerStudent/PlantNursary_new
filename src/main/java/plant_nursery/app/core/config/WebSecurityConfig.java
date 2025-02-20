90b2be28ea6ef96e7093789191e9530e0d2976f2
MirGerStudent <mirger140503@gmail.com>
2025-02-20 15:57:52 +0600

    [SecurityConfig] create and add config with erorr

---
+++ src/main/java/plant_nursery/app/core/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package plant_nursery.app.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/home.html")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
